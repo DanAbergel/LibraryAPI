89a6284b805bc7bb25e012718bf21cb05bfa2288
Dan Abergel <danabergel1995@gmail.com>
2025-02-19 23:06:42 +0200

    Delete book-service directory

--- book-service/app/databases/settings.py
+++
@@ -1,1 +0,0 @@
-uri = "mongodb+srv://danabergel1995:DA300722%23@clusterlibrary.rkcdzec.mongodb.net/?retryWrites=true&w=majority&appName=ClusterLibrary"
--- book-service/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes.route import router
-
-app = FastAPI(
-    title="Library Manager",
-    summary="Application which manages a library , and makes easier to order a book online"
-)
-
-app.include_router(router)
